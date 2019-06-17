f8a02c18ecafee9eedca4e35dd950eeee856b77d
zeropercenthappy <zeropercenthappy@foxmail.com>
2019-06-17 15:50:53 +0800

    update kotlin version. clear useless dependencies. downgrade minSdkVersion to 14.

--- glideutil/src/main/java/com/zeropercenthappy/glideutil/GlideUtils.kt
+++ glideutil/src/main/java/com/zeropercenthappy/glideutil/GlideUtils.kt
@@ -3,89 +3,89 @@ package com.zeropercenthappy.glideutil
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
-import org.jetbrains.anko.doAsync
 import java.io.File
+import kotlin.concurrent.thread
 
 object GlideUtils {
 
     @JvmStatic
     fun loadImage(context: Context, url: String, imageView: ImageView) {
         GlideApp.with(context)
-                .load(url)
-                .into(imageView)
+            .load(url)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, url: String, imageView: ImageView, placeholder: Int) {
         GlideApp.with(context)
-                .load(url)
-                .placeholder(placeholder)
-                .error(placeholder)
-                .into(imageView)
+            .load(url)
+            .placeholder(placeholder)
+            .error(placeholder)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, url: String, imageView: ImageView, placeholder: Drawable) {
         GlideApp.with(context)
-                .load(url)
-                .placeholder(placeholder)
-                .error(placeholder)
-                .into(imageView)
+            .load(url)
+            .placeholder(placeholder)
+            .error(placeholder)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, file: File, imageView: ImageView) {
         GlideApp.with(context)
-                .load(file)
-                .into(imageView)
+            .load(file)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, file: File, imageView: ImageView, placeholder: Int) {
         GlideApp.with(context)
-                .load(file)
-                .placeholder(placeholder)
-                .error(placeholder)
-                .into(imageView)
+            .load(file)
+            .placeholder(placeholder)
+            .error(placeholder)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, file: File, imageView: ImageView, placeholder: Drawable) {
         GlideApp.with(context)
-                .load(file)
-                .placeholder(placeholder)
-                .error(placeholder)
-                .into(imageView)
+            .load(file)
+            .placeholder(placeholder)
+            .error(placeholder)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, res: Int, imageView: ImageView) {
         GlideApp.with(context)
-                .load(res)
-                .into(imageView)
+            .load(res)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, res: Int, imageView: ImageView, placeholder: Int) {
         GlideApp.with(context)
-                .load(res)
-                .placeholder(placeholder)
-                .error(placeholder)
-                .into(imageView)
+            .load(res)
+            .placeholder(placeholder)
+            .error(placeholder)
+            .into(imageView)
     }
 
     @JvmStatic
     fun loadImage(context: Context, res: Int, imageView: ImageView, placeholder: Drawable) {
         GlideApp.with(context)
-                .load(res)
-                .placeholder(placeholder)
-                .error(placeholder)
-                .into(imageView)
+            .load(res)
+            .placeholder(placeholder)
+            .error(placeholder)
+            .into(imageView)
     }
 
     @JvmStatic
     fun clearCache(context: Context) {
-        doAsync {
+        thread {
             GlideApp.get(context).clearDiskCache()
         }
     }
